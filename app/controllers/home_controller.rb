38428f2c4a96a9f543b102b4b769a61a449ab885
Ivan Medina <medinadiazivan@gmail.com>
2018-07-08 13:22:36 -0500

    sets leaderboard view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @users = User.ranking
   end
 
   def history
     @games = Game.where(user_id: current_user.id)
-    p @games
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :trackable, :validatable
 
   scope :opponents, ->(user) { where.not(id: user) }
+  scope :games_played, ->(user) { Game.where(user_id: user).count }
 
-  # def opponents
+  scope :ranking, lambda { User.all.sort_by(&:score).reverse }
 
-  # end
+  def score
+    won_games = Game.where("user_score > opponent_score AND user_id = :id", { id: self.id })
+    lost_games = Game.where("user_score < opponent_score AND user_id = :id", { id: self.id })
+
+    return (won_games.count * 100) - (lost_games.count * -50)
+  end
+
 end
